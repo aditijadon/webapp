f153f6f766a49fc5d8d1ab255f845e40f9e47198
aditijadon28 <aditijadon28@gmail.com>
2020-01-25 14:10:22 +0530

    using external tomcat and java to create PingServlet

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,14 +4,12 @@ import java.sql.ResultSet;
 public class Driver {
     public static void main(String[] args) {
 
-        System.out.println("Hello World, Maven");
-
-       /* Class cls = ResultSet.class;
+        Class cls = ResultSet.class;
         Method[] methods = cls.getMethods();
 
         System.out.println(cls.getName() + " is accessible here and has following methods.");
         for (Method method : methods) {
             System.out.println(method.getName());
-        } */
+        }
     }
 }
