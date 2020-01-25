65f37fc1f753baefc9d47fe9e25fbcc5617b8078
aditijadon28 <aditijadon28@gmail.com>
2020-01-25 12:14:11 +0530

    Use of maven to add dependenies and to build jar

---
+++ src/main/java/Driver.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World, Maven");
+
+       /* Class cls = ResultSet.class;
+        Method[] methods = cls.getMethods();
+
+        System.out.println(cls.getName() + " is accessible here and has following methods.");
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        } */
+    }
+}
